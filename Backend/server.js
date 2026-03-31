42753b4d8b1f7c36d0b9d923e5e909dc93a9727a
hinojosacarlose56-ui <hinojosa.carlos.e56@gmail.com>
2026-03-30 23:12:14 -0500

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -85,17 +85,6 @@ const puppies =sequelize.define('puppies', {
   timestamps: false,
 });
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
 
 //Add in the CRUD functions: 
 app.get('/puppies', async (req, res) => {
